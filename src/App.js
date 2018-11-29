7e5b8f4060f425cc969da84068c38e73707ab45a
Albertus Angga Raharja <albertusangga.98@gmail.com>
2018-11-29 20:43:40 +0700

    Add modal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import CongklakBoard from "./components/CongklakBoard";
 import { DIFFICULTY } from "./logic/congklakDifficulty";
@@ -9,13 +9,13 @@ export default function App(props) {
   const [isGameStarted, setIsGameStarted] = useState(false);
   const [delay, setDelay] = useState(250);
   const [difficulty, setDifficulty] = useState(DIFFICULTY.EASY);
-
-  useEffect(() => {
-    document.title = "Congklak.AI";
-  });
+  const [isDifficultyChanged, setDifficultyChanged] = useState(false);
+  const [isScoreboardShown, setIsScoreboardShown] = useState(false);
+  const [isHowToShown, setIsHowToShown] = useState(false);
 
   function handleClickStartButton() {
     setIsGameStarted(!isGameStarted);
+    setDifficultyChanged(true);
   }
 
   function handleSetDelay(e) {
@@ -28,6 +28,14 @@ export default function App(props) {
 
   function handleSetUsername(e) {
     setUsername(e.target.value);
+  }
+
+  function toggleScoreboard() {
+    setIsScoreboardShown(!isScoreboardShown);
+  }
+
+  function toggleHowTo() {
+    setIsHowToShown(!isHowToShown);
   }
 
   return (
@@ -55,53 +63,57 @@ export default function App(props) {
               value={delay}
               onChange={handleSetDelay}
             />
-            <h5 style={{ marginTop: "1.5rem" }}>Choose Difficulty</h5>
-            <form
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-between",
-                alignItems: "flex-start",
-                width: "20rem"
-              }}
-            >
-              <label>
-                <input
-                  type="radio"
-                  value={DIFFICULTY.EASY}
-                  onChange={handleDifficulty}
-                  checked={difficulty === DIFFICULTY.EASY}
-                />
-                <span>Easy</span>
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  value={DIFFICULTY.MEDIUM}
-                  onChange={handleDifficulty}
-                  checked={difficulty === DIFFICULTY.MEDIUM}
-                />
-                <span>Medium</span>
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  value={DIFFICULTY.HARD}
-                  onChange={handleDifficulty}
-                  checked={difficulty === DIFFICULTY.HARD}
-                />
-                <span>Hard</span>
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  value={DIFFICULTY.BRUTAL}
-                  onChange={handleDifficulty}
-                  checked={difficulty === DIFFICULTY.BRUTAL}
-                />
-                <span>Brutal</span>
-              </label>
-            </form>
+            {!isDifficultyChanged && (
+              <React.Fragment>
+                <h5 style={{ marginTop: "1.5rem" }}>Choose Difficulty</h5>
+                <form
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                    alignItems: "flex-start",
+                    width: "20rem"
+                  }}
+                >
+                  <label>
+                    <input
+                      type="radio"
+                      value={DIFFICULTY.EASY}
+                      onChange={handleDifficulty}
+                      checked={difficulty === DIFFICULTY.EASY}
+                    />
+                    <span>Easy</span>
+                  </label>
+                  <label>
+                    <input
+                      type="radio"
+                      value={DIFFICULTY.MEDIUM}
+                      onChange={handleDifficulty}
+                      checked={difficulty === DIFFICULTY.MEDIUM}
+                    />
+                    <span>Medium</span>
+                  </label>
+                  <label>
+                    <input
+                      type="radio"
+                      value={DIFFICULTY.HARD}
+                      onChange={handleDifficulty}
+                      checked={difficulty === DIFFICULTY.HARD}
+                    />
+                    <span>Hard</span>
+                  </label>
+                  <label>
+                    <input
+                      type="radio"
+                      value={DIFFICULTY.BRUTAL}
+                      onChange={handleDifficulty}
+                      checked={difficulty === DIFFICULTY.BRUTAL}
+                    />
+                    <span>Brutal</span>
+                  </label>
+                </form>
+              </React.Fragment>
+            )}
             <div
               style={{
                 marginTop: "1rem",
@@ -113,10 +125,7 @@ export default function App(props) {
             >
               <h5
                 for="first_name"
-                style={{
-                  fontWeight: "bold",
-                  color: "black"
-                }}
+                style={{ fontWeight: "bold", color: "black" }}
               >
                 Your Name
               </h5>
@@ -139,58 +148,69 @@ export default function App(props) {
         />
       </div>
 
-      <h4>Scoreboard</h4>
-      <ScoreboardList />
-
       <div className="buttondiv">
         <button className="btn start-button" onClick={handleClickStartButton}>
           {isGameStarted ? "Change Config" : "Start Game"}
         </button>
+        <button className="btn orange lighten-2" onClick={toggleScoreboard}>
+          See Scoreboard
+        </button>
+        <button className="btn" onClick={toggleHowTo}>
+          How to Play
+        </button>
       </div>
 
-      <div className="how-to">
-        <div className="ht-clicker" tabIndex="1">
-          <h5>How to Play</h5>
+      {isScoreboardShown && <ScoreboardList toggle={toggleScoreboard} />}
+
+      {isHowToShown && (
+        <div className="how-to-wrapper">
+          <div className="how-to-modal-wrapper" onClick={toggleHowTo} />
+          <div className="card how-to-card-wrapper">
+            <div className="card-content">
+              <h3 className="card-content-title">How To Play</h3>
+              <ol>
+                <li>
+                  Player chooses one of the seven holes on their side, which is
+                  bottom side. There is no time limit for player on each turn to
+                  choose the hole.
+                </li>
+                <li>
+                  Automatically the seeds in the hole will be inserted one by
+                  one into each small hole and large hole on the right side of
+                  the board with a counter-clockwise movement.
+                </li>
+                <li>
+                  If the last seed drops in the hole on the player's side
+                  (bottom side) and the hole is not empty, then all the seeds in
+                  the hole are automatically taken and the game will repeat step
+                  (2).
+                </li>
+                <li>
+                  If the last seed drops in the hole on the player's side
+                  (bottom side) and the hole is empty, then automatically all
+                  seeds in the opposite hole (opponent's side) will be inserted
+                  into the player's large hole (right side). Then the player's
+                  turn has finished and then it is the turn of the bot.
+                </li>
+                <li>
+                  If the last seed enters the player's large hole (right side),
+                  the player can continue the game by repeating step (1).
+                </li>
+                <li>
+                  If the last seed enters the opponent's side (upper side), then
+                  the player's turn has finished and then it is the turn of the
+                  bot.
+                </li>
+                <li>
+                  The game ends when there is no longer any seeds in small
+                  holes. The player who manages to collect the most seeds is the
+                  winner.
+                </li>
+              </ol>
+            </div>
+          </div>
         </div>
-        <div className="ht-hiddendiv">
-          <ol>
-            <li>
-              Player chooses one of the seven holes on their side, which is
-              bottom side. There is no time limit for player on each turn to
-              choose the hole.
-            </li>
-            <li>
-              Automatically the seeds in the hole will be inserted one by one
-              into each small hole and large hole on the right side of the board
-              with a counter-clockwise movement.
-            </li>
-            <li>
-              If the last seed drops in the hole on the player's side (bottom
-              side) and the hole is not empty, then all the seeds in the hole
-              are automatically taken and the game will repeat step (2).
-            </li>
-            <li>
-              If the last seed drops in the hole on the player's side (bottom
-              side) and the hole is empty, then automatically all seeds in the
-              opposite hole (opponent's side) will be inserted into the player's
-              large hole (right side). Then the player's turn has finished and
-              then it is the turn of the bot.
-            </li>
-            <li>
-              If the last seed enters the player's large hole (right side), the
-              player can continue the game by repeating step (1).
-            </li>
-            <li>
-              If the last seed enters the opponent's side (upper side), then the
-              player's turn has finished and then it is the turn of the bot.
-            </li>
-            <li>
-              The game ends when there is no longer any seeds in small holes.
-              The player who manages to collect the most seeds is the winner.
-            </li>
-          </ol>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,54 +1,112 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { DASHBOARD_URL } from "../urls";
+import "./Scoreboard.css";
+import Spinner from "react-spinkit";
 
-export default function ScoreboardList(props) {
-  const [easy, setEasy] = useState([]);
-  const [medium, setMedium] = useState([]);
-  const [hard, setHard] = useState([]);
-  const [brutal, setBrutal] = useState([]);
+export default class ScoreboardList extends React.Component {
+  state = {
+    data: [],
+    selectedDifficulty: "EASY",
+    loading: true
+  };
 
-  const queryDifficulty = difficulty => `?difficulty=${difficulty}`;
+  queryDifficulty = difficulty => `?difficulty=${difficulty}`;
 
-  useEffect(async () => {
-    const easy = await axios(DASHBOARD_URL + queryDifficulty("EASY"));
-    const medium = await axios(DASHBOARD_URL + queryDifficulty("MEDIUM"));
-    const hard = await axios(DASHBOARD_URL + queryDifficulty("HARD"));
-    const brutal = await axios(DASHBOARD_URL + queryDifficulty("BRUTAL"));
+  async fetch(difficulty) {
+    this.setState({ loading: true });
+    const { data } = await axios(
+      DASHBOARD_URL + this.queryDifficulty(difficulty)
+    );
+    this.setState({
+      data,
+      loading: false
+    });
+  }
 
-    setEasy(easy);
-    setMedium(medium);
-    setHard(hard);
-    setBrutal(brutal);
+  componentDidMount() {
+    this.fetch(this.state.selectedDifficulty);
+  }
 
-    console.log("Easy", easy);
-    console.log("Medium", medium);
-    console.log("Hard", hard);
-    console.log("Brutal", brutal);
-  });
+  handleButtonClick = difficulty => () => {
+    this.fetch(difficulty);
+    console.log(difficulty);
+  };
 
-  return (
-    <div>
-      {easy.map(scoreboard => (
-        <pre>
-          <code>{JSON.stringify(scoreboard, 2, 2)}</code>
-        </pre>
-      ))}
-      {medium.map(scoreboard => (
-        <pre>
-          <code>{JSON.stringify(scoreboard, 2, 2)}</code>
-        </pre>
-      ))}
-      {hard.map(scoreboard => (
-        <pre>
-          <code>{JSON.stringify(scoreboard, 2, 2)}</code>
-        </pre>
-      ))}
-      {brutal.map(scoreboard => (
-        <pre>
-          <code>{JSON.stringify(scoreboard, 2, 2)}</code>
-        </pre>
-      ))}
-    </div>
-  );
+  render() {
+    const { easy, medium, hard, brutal } = this.state;
+
+    return (
+      <div className="scoreboard-wrapper">
+        <div className="scoreboard-modal-wrapper" onClick={this.props.toggle} />
+        <div className="card scoreboard-card-wrapper">
+          <div className="card-content">
+            <span className="card-title scoreboard-title">Scoreboard</span>
+            <div className="scoreboard-button-wrapper">
+              <button
+                className="btn blue"
+                onClick={this.handleButtonClick("EASY")}
+              >
+                Easy
+              </button>
+              <button
+                className="btn green"
+                onClick={this.handleButtonClick("MEDIUM")}
+              >
+                Medium
+              </button>
+              <button
+                className="btn yellow"
+                onClick={this.handleButtonClick("HARD")}
+              >
+                Hard
+              </button>
+              <button
+                className="btn red"
+                onClick={this.handleButtonClick("BRUTAL")}
+              >
+                Brutal
+              </button>
+            </div>
+            <div
+              className={
+                this.state.loading
+                  ? "result-wrapper custom-spinner"
+                  : "result-wrapper"
+              }
+            >
+              {this.state.loading && (
+                <Spinner
+                  name="circle"
+                  color="#795548"
+                  className="custom-spinner"
+                  style={{ justifyContent: "center" }}
+                />
+              )}
+              {!this.state.loading && (
+                <table>
+                  <thead>
+                    <tr>
+                      <td>Rank</td>
+                      <td>Score</td>
+                      <td>Name</td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.state.data.map((datum, idx) => (
+                      <tr>
+                        <td>{idx + 1}</td>
+                        <td>{datum.score}</td>
+                        <td>{datum.name}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
